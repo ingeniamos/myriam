1f23b9b76d7c85dfec1bffea786c942703b5bf04
Gabriel Gamboa <sinapsis.513@gmail.com>
2018-05-17 00:17:43 -0500

    Subir

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,138 +5,186 @@
 @include('alerts.errors')
 @include('alerts.request')
 
-<div class="header">
-	<img class="img-responsive" src="images/m1.jpg?v=003" class="img-responsive">
+<div class="row" style="display: block;">
+	<img class="img-responsive" src="images/m1.jpg?v=003">
 </div>
 
-<div class="review-slider">
-	 <ul id="flexiselDemo1">
-	<li><img src="images/voto1.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto2.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto3.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto4.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto5.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto6.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto7.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto8.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto9.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto10.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto11.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto12.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto13.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto14.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto15.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto16.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto17.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto18.jpg?v=001" alt=""/></li>
-	<li><img src="images/voto19.jpg?v=001" alt=""/></li>
-</ul>
-	<script type="text/javascript">
-$(window).load(function() {
-	
-  $("#flexiselDemo1").flexisel({
-		visibleItems: 6,
-		animationSpeed: 1000,
-		autoPlay: true,
-		autoPlaySpeed: 3000,    		
-		pauseOnHover: false,
-		enableResponsiveBreakpoints: true,
-		responsiveBreakpoints: { 
-			portrait: { 
-				changePoint:480,
-				visibleItems: 2
-			}, 
-			landscape: { 
-				changePoint:640,
-				visibleItems: 3
-			},
-			tablet: { 
-				changePoint:768,
-				visibleItems: 4
-			}
-		}
-	});
-	});
-</script>
-<script type="text/javascript" src="js/jquery.flexisel.js"></script>	
-</div>
-
-<div class="col-md-12 titulo">
-	<h2>Hola amigas y amigos!</h2>
-	<h2>Los invito a conocer mis</h2>
-	<h1 style="color: #073692"><b>COMPROMISOS</b></h1>
-</div>
-
-<div class="news">
-	<div class="col-md-6 news-left-grid">
-		<h1 style="color: #073692"><b>PROGRAMA</b></h1>
-		<div class="news-grid">
-			<p>
-				Con base en 4 ejes programáticos fundamentales voy a formular, presentar y gestionar proyectos de Ley ante el Congreso de la República que mejoren la productividad, la educación, la situación de la mujer y la protección del medio ambiente, en el fin supremo de contribuir a un verdadero desarrollo regional.
-			</p><p>
-				También en función de 4 ejes transversales voy a apoyar, defender y patrocinar Proyectos de Ley que contribuyan a mejorar la oferta de salud, la implementación de los acuerdos de paz dentro del marco de la ley, el cuidado del patrimonio de los colombianos y el manejo proactivo de la inmigración de venezolanos a nuestro país. 
-			</p><p>
-				Me comprometo específicamente:
-			</p>
+<div class="row">
+	<div class="col">
+		<div class="jumbotron" style="background-color: white;">
+		  <h2 class="display-4">Experiencia académica</h2>
+		   <hr class="my-4">
+		  <table class="table table-striped" style="font-size: 1.6rem; margin: 30px 0 0">
+		  	<tr>
+		  		<td>
+		  			Intensidad Horaria
+		  		</td>
+		  		<td>
+		  			120 horas
+		  		</td>
+		  	</tr>
+		  	<tr>
+		  		<td>
+		  			Horarios
+		  		</td>
+		  		<td>
+		  			Viernes 6:00  a 10:00 pm –  Sábado 7:00 a.m. – 1:00 pm  
+		  		</td>
+		  	</tr>
+		  	<tr>
+		  		<td>
+		  			Inicio
+		  		</td>
+		  		<td>
+		  			27 de julio de 2018
+		  		</td>
+		  	</tr>
+		  	<tr>
+		  		<td>
+		  			Finaliza
+		  		</td>
+		  		<td>
+		  			13 de octubre de 2018
+		  		</td>
+		  	</tr>
+		  </table>
 		</div>
-
-		<div class="news-grid">
-			<h5>Conciencia de región</h5>
-			<p>A promover y liderar "conciencia de región" con los gobernantes y los ciudadanos para sacar adelante proyectos de ley que nos beneficien, independientemente de la ideología política.</p>
-		</div>
-		<div class="news-grid">
-			<h5>Equipo comprometido</h5>
-			<p>A formar un equipo de profesionales idóneos para desde ya activar, dar a conocer, y sacarle el máximo provecho a las leyes ya aprobadas que facilitan nuestro impulso al desarrollo.</p>
-		</div>
-		<div class="news-grid">
-			<h5>Control político</h5>
-			<p>A ejercer control político en las leyes que puedan perjudicar a la comunidad, y a apoyar las que conlleven a favorecerla.</p>
-		</div>
-		<!-- <a class="more" href="#">Ver todo el programa</a> -->
 	</div>
-
-	<div class="col-md-6 news-right-grid">
-		<h1 style="color: #073692"><b>MI PERFIL</b></h1>
-		<div class="news-grid">
-			<p>
-				Soy Myriam Elsy Camargo de Gamboa, casada con el ingeniero civil Álvaro Fernando Gamboa, madre de dos hijas profesionales.
-			</p>
-		</div>
-
-		<div class="news-grid">
-			<h5>Una mujer preparada</h5>
-			<p>
-				- Especialista en finanzas de la EAFIT <br>
-				- Contadora púbica de la UFPS
-			</p>
-		</div>
-		<div class="news-grid">
-			<h5>Con vocación docente</h5>
-			<p>
-				- Docente universitaria por más de 25 años<br>
-				- Decana de la facultad de ciencias empresariales de la UFPS<br>
-				- Líder en la creación y puesta en  marcha de la carrera negocios internacionales de la UFPS
-			</p>
-		</div>
-		<div class="news-grid">
-			<h5>Experiencia administrativa</h5>
-			<p>
-				- Directora del ICETEX. Gestionó becas de manutención con las alcaldías del departamento para jóvenes de estrato 1 y 2.<br>
-				- Asesora financiera y comercial de empresas importantes de la región
-			</p>
-		</div>
-		<div class="news-grid">
-			<h5>y sentido social</h5>
-			<p>
-				- Líder social para el empoderamiento de la mujer en Norte de Santander.
-			</p>
-		</div>
-		
-	</div>
-
-	<div class="clearfix"></div>
-	
 </div>
+
+<div class="row">
+	<div class="col">
+		<div class="jumbotron">
+		  <h2 class="display-4">Inversión</h2>
+		  <hr class="my-4">
+
+		  <div class="row">
+			<div class="col-md-4 col-12">
+				<div class="card text-white bg-primary mb-3" style="width: 100%;">
+				  <div class="card-header">PREVENTA 30%</div>
+				  <div class="card-body">
+				    <h5 class="card-title">Hasta el 25 de mayo de 2018</h5>
+				    <p class="card-text">$2.800.000</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 col-12">
+				<div class="card text-white bg-success mb-3" style="width: 100%;">
+				  <div class="card-header">PREVENTA 25%</div>
+				  <div class="card-body">
+				    <h5 class="card-title">Hasta el 30 de junio de 2018</h5>
+				    <p class="card-text">$2.950.000</p>
+				  </div>
+				</div>
+			</div>
+			<div class="col-md-4 col-12">
+				<div class="card text-white bg-secondary mb-3" style="width: 100%;">
+				  <div class="card-header">PREVENTA 20%</div>
+				  <div class="card-body">
+				    <h5 class="card-title">Hasta el 15 de julio de 2018</h5>
+				    <p class="card-text">$3.200.000</p>
+				  </div>
+				</div>
+			</div>
+		  </div>
+		  <h2 class="display-4 text-center">Valor sin descuentos: $3.927.000</h2>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<div class="jumbotron" style="background-color: aliceblue;">
+		  <h2 class="display-4">¿Quiénes somos?</h2>
+		  <hr class="my-4">
+		  <p>
+		  	<b>CONSTRUPRECIOS</b> “El informe de los precios de construcción”, es una empresa colombiana especializada desde hace más de 15 años en el área de costos de construcción y que ha suministrado al país la herramientas para la elaboración de presupuestos mejor posicionada en la región Nororiental de Colombia; analizando las deficiencias presentes en el gremio para la elaboración de presupuestos, Construprecios presenta su primer diplomado en presupuesto, programación y control de obra. <br><br>
+		  	<b>Nuestro objetivo</b> es dar a conocer herramientas útiles para realizar una adecuada elaboración  de presupuestos, programación y control de obra; con el fin de cumplir metas y objetivos en un tiempo determinado. 
+		  </p>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<div class="jumbotron" style="background-color: white;">
+		  <h2 class="display-4">Generalidades del Diplomado</h2>
+		  <hr class="my-4">
+		  <p>
+		  </p>
+
+		<div class="accordion" id="accordion">
+		  <div class="card">
+		    <div class="card-header" id="headingOne">
+		      <h5 class="mb-0">
+		        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+		          ¿Quién debe aprovechar esta oportunidad?
+		        </button>
+		      </h5>
+		    </div>
+
+		    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+		      <div class="card-body">
+		       El diplomado está dirigido a ingenieros civiles, arquitectos, constructores, contratistas, interventores, consultores, profesionales de áreas afines y relacionadas con el sector constructor, que ejerzan actividades de gestión de proyectos, con poca experiencia de campo o que quieran mejorar su desempeño en la elaboración de presupuesto, programación y control de obra.
+		      </div>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <div class="card-header" id="headingTwo">
+		      <h5 class="mb-0">
+		        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+		          Requisitos mínimos
+		        </button>
+		      </h5>
+		    </div>
+		    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+		      <div class="card-body">
+		      	<ol>
+					<li>Formulario de inscripción diligenciado y con firma.</li>
+					<li>2 fotografías 3×4 fondo azul.</li>
+					<li>Fotocopia de la cédula de ciudadanía ampliada al 150% o equivalente según el país de origen del estudiante, pasaporte y visa.</li>
+					<li>Consignación del pago de matrícula.</li>
+					<li>Fotocopia de la tarjeta profesional, diploma o acta de grado; o constancia de la universidad que acredite que está cursando 9 semestre.</li>
+					<li>Resumen de la hoja de vida del aspirante.</li>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <div class="card-header" id="headingThree">
+		      <h5 class="mb-0">
+		        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+		          Certificación
+		        </button>
+		      </h5>
+		    </div>
+		    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+		      <div class="card-body">
+				Se certifica a quienes cumplan con el 85% de asistencia y que obtengan un promedio igual o superior a 3.5.
+			</div>
+		    </div>
+		  </div>
+		  <div class="card">
+		    <div class="card-header" id="headingOne">
+		      <h5 class="mb-0">
+		        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+		          Metodología
+		        </button>
+		      </h5>
+		    </div>
+
+		    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+		      <div class="card-body">
+				El diplomado se desarrollará con conferencias y sesiones teórico-prácticas, apoyadas en medios audiovisuales a través de los cuales los docentes presentarán  los conceptos y contenidos de cada tema reforzado con el análisis de casos reales, además de desarrollar ejercicios prácticos con la asistencia de los docentes que permitan la completa comprensión de los temas.
+			</div>
+		    </div>
+		  </div>		  
+		</div>		  
+
+		</div>
+	</div>
+</div>
+
+
 <div class="video">
 	<iframe  src="https://www.youtube.com/embed/2LqzF5WauAw" frameborder="0" allowfullscreen></iframe>
 </div>
--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -12,28 +12,32 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Myriam de Gamboa, Representante a la Cámara, Norte de Santander, Partido Conservador, Congreso 2018, Docente UFPS, Congreso Visible, Myriam Camargo de Gamboa, Congreso Cúcuta, Senado de la República" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link href="css/mystyle.css?v=001" rel="stylesheet" type="text/css" media="all" />
 
 </head>
 <body>
+	<div class="container-fluid">
+		@yield('content')
+	</div>
 	<!-- header-section-starts -->
 	<div class="full">
-			{{-- <div class="menu">
-				<ul>
-					<li><a class="active" href="index.html"><i class="home"></i></a></li>
-					<li><a href="videos.html"><div class="video"><i class="videos"></i><i class="videos1"></i></div></a></li>
-					<li><a href="reviews.html"><div class="cat"><i class="watching"></i><i class="watching1"></i></div></a></li>
-					<li><a href="404.html"><div class="bk"><i class="booking"></i><i class="booking1"></i></div></a></li>
-					<li><a href="contact.html"><div class="cnt"><i class="contact"></i><i class="contact1"></i></div></a></li>
-				</ul>
-			</div> --}}
-	<div class="main">
+				{{-- <div class="menu">
+					<ul>
+						<li><a class="active" href="index.html"><i class="home"></i></a></li>
+						<li><a href="videos.html"><div class="video"><i class="videos"></i><i class="videos1"></i></div></a></li>
+						<li><a href="reviews.html"><div class="cat"><i class="watching"></i><i class="watching1"></i></div></a></li>
+						<li><a href="404.html"><div class="bk"><i class="booking"></i><i class="booking1"></i></div></a></li>
+						<li><a href="contact.html"><div class="cnt"><i class="contact"></i><i class="contact1"></i></div></a></li>
+					</ul>
+				</div> --}}
+		<div class="main">
 
-	@yield('content')
 		
-	<div class="footer">
-		<p>Todos los derechos reservados - 2018</p>
-	</div>	
-	</div>
+			
+		<div class="footer">
+			<p>Todos los derechos reservados - 2018</p>
+		</div>	
+		</div>
 	</div>
 	<div class="clearfix"></div>
 	<script src="http://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
